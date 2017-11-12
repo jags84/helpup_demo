2eab84a452f36b0b6e01d7e0949b393ce88ade7c
Juan Gonzalez <jags84@gmail.com>
2017-11-13 00:08:46 +0100

    Uploader Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :albums, only: [:index,:show,:create]
+  resources :pictures, only: [:create,:destroy]
   root to: 'home#index'
 end
