9a0fc98c76e0b08edbbc3829ecfbfd12c7b3b817
Juan Gonzalez <jags84@gmail.com>
2017-11-13 19:26:48 +0100

    Factories

--- spec/factories/pictures.rb
+++ spec/factories/pictures.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :picture do
-    album nil
+    album
+    photo { Rack::Test::UploadedFile.new(Rails.root.join('app', 'assets', 'images', 'empty_album.png'), 'image/png') }
   end
 end
